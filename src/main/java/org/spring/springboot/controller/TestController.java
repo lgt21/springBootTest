d4b52ca14d1f07d374238a9f16831b563e7a6bc8
liguitao <liguitao@totaldata.cn>
2017-08-18 18:27:25 +0800

    init

---
+++ src/main/java/org/spring/springboot/controller/TestController.java
@@ -0,0 +1,22 @@
+package org.spring.springboot.controller;
+
+import org.spring.springboot.domain.City;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * User: lgt
+ * Date: 2017/8/18  18:18
+ */
+@RestController
+@RequestMapping(value = "test")
+public class TestController {
+
+    @RequestMapping(value = "")
+    private City test() {
+        City city = new City();
+        city.setCityName("测试");
+        return city;
+    }
+}

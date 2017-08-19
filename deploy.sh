4bacda2edfb2bfae021348cc9c2fc853d8f26c3c
lgt <test@mail.com>
2017-08-20 01:44:38 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set tomcat_root = /opt/tomcat-dev
-${tomcat_root}/opt/tomcat_dev/bin/shutdown.sh
+${tomcat_root}/bin/shutdown.sh
 kill -9 `ps -ef | grep tomcat_dev | awk '{print $2}''`
 cp springBootTest.war ${tomcat_root}/webapps/
-${tomcat_root}/opt/tomcat_dev/bin/startup.sh
+${tomcat_root}/bin/startup.sh
